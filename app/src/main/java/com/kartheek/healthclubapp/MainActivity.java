40f20dca93dddabb79f9a4052a731ab2926015b6
kartheek <kartheek222@gmail.com>
2015-08-12 10:48:47 +0530

    Initial commit

---
+++ app/src/main/java/com/kartheek/healthclubapp/MainActivity.java
@@ -0,0 +1,19 @@
+package com.kartheek.healthclubapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void openTask1(View view) {
+
+    }
+}
---
+++ app/src/main/java/com/kartheek/healthclubapp/task1/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.kartheek.healthclubapp.task1;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+import com.kartheek.healthclubapp.R;
+
+public class LoginActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
